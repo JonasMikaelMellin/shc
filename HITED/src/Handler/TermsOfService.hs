4ac72b991c7e48daf2112f5fa3b340542d2ad51f
JonasMikaelMellin <jonas.mellin@his.se>
2018-11-13 11:01:21 +0100

    Privacy policy + terms of service

---
+++ HITED/src/Handler/TermsOfService.hs
@@ -0,0 +1,42 @@
+-- |
+-- Module      : Handler.TermsOfService
+-- Copyright   : (c) 2018 Jonas Mellin
+--
+-- License     : GPLv3
+--
+-- Maintainer  : jonas.mellin@his.se
+-- Stability   : experimental
+-- Portability : Yesod
+--
+--
+-- This handler module implements the back end functionality of team management in the
+-- HITED software.
+
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE TypeFamilies #-}
+module Handler.TermsOfService where
+
+import Import
+import Yesod.Form.Bootstrap3 (BootstrapFormLayout (..), renderBootstrap3)
+import Text.Julius (RawJS (..))
+
+
+-- This is a handler function for the GET request method on the PrivacyPolicyR
+-- resource pattern. All of your resource patterns are defined in
+-- config/routes
+--
+-- The majority of the code you will write in Yesod lives in these handler
+-- functions. You can spread them across multiple files if you are so
+-- inclined, or create a single monolithic file.
+getTermsOfServiceR :: Handler Html
+getTermsOfServiceR = do
+    setLanguage "sv"
+    master <- getYesod
+    defaultLayout $ do
+        setTitle $ toHtml $ renderMessage master [] MsgTermsOfServiceRTitle
+        $(widgetFile "termsOfService")
+
+ 
